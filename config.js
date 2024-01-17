4e049b7d31d8d59390d1a3eb156bb19aaf20b05e
Rexandre99 <156932592+Rexandre99@users.noreply.github.com>
2024-01-17 17:19:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349061789617', 'Rex', true],
+  ['9061789617', 'Andre', true], 
   [''] 
 ] //Number of owners
 
